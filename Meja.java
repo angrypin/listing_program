5af06d1a62a6ab511ec7c5e10173195082928213
angrypin <arifinmblygf@gmail.com>
2021-09-13 13:10:29 +0700

    listing program kelas Meja

---
+++ Meja.java
@@ -0,0 +1,23 @@
+/* M. Arifin Ilham - XII RPL 1 - 13/09/2021 */
+
+// kelas Meja
+public class Meja {
+  // fungsi utama
+  public static void main(String[] args) {
+    // variable untuk Meja
+    int kaki = 4;
+    String
+      bahan = "Kayu",
+      warna = "Coklat",
+      bentuk = "Persegi Panjang";
+
+    // menampilkan 4 ciri dari kelas Meja
+    System.out.print(
+      "Ciri - ciri Meja" +
+      "\n- Jumlah kaki\t: " + kaki +
+      "\n- Warna\t\t: " + warna +
+      "\n- Bahan\t\t: " + bahan +
+      "\n- bentuk meja\t: " + bentuk + "\n"
+    );
+  }
+}
